67efff8a5c2ac327d0889af4863fd7488bcc8c88
surivin <35077826+surivin@users.noreply.github.com>
2024-07-07 17:03:34 -0600

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace AzdoCommands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        XmlDocument doc = new XmlDocument();
+        public MainWindow()
+        {
+            InitializeComponent();
+            doc.Load("Commands.xml");
+            BindCommandsCmb();
+            BindEnvironmentCmb();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            output.Text = "";
+            var command = GetCommandFromXML(environmentCmb.SelectedItem.ToString());
+
+            Response result = Shell.Term($"echo hello & pause & echo hello again", Output.Internal);
+            if (result.code == 0)
+            {
+                output.Text = (result.stdout);
+            }
+            else
+            {
+                output.Text = (result.stderr);
+            }
+        }
+
+        private string GetCommandFromXML(string environment)
+        {
+            foreach (XmlNode env in doc.ChildNodes[0].ChildNodes)
+            {
+                if (env.Attributes["name"].Value.ToLower() == environment.Split('-')[0].ToLower())
+                {
+                    foreach (XmlNode command in env.ChildNodes)
+                    {
+                        if (command.Attributes["name"].Value.ToLower() == commandCmb.Text.ToLower())
+                        {
+                            return command.InnerText.Replace("[Env]", environment);
+                        }
+                    }
+                }
+            }
+            return "";
+        }
+
+        private void BindEnvironmentCmb()
+        {
+            var env = ConfigurationManager.AppSettings["Environments"].ToString();
+
+            environmentCmb.DataContext = env.Split(',');            
+        }
+
+        private void BindCommandsCmb()
+        {
+            var commands = ConfigurationManager.AppSettings["Commands"].ToString();
+
+            commandCmb.DataContext = commands.Split(',');
+        }
+    }
+}
---
+++ RunConsoleCommand.cs
@@ -0,0 +1,118 @@
+﻿
+using System.Runtime.InteropServices;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+using System.Reflection;
+using System.IO;
+
+namespace AzdoCommands
+{
+    public class Response
+    {
+        public int code { get; set; }
+        public string stdout { get; set; }
+        public string stderr { get; set; }
+    }
+
+    public enum Output
+    {
+        Hidden,
+        Internal,
+        External
+    }
+
+    public static class Shell
+    {
+        private static string GetFileName()
+        {
+            return "cmd.exe";
+        }
+
+
+        private static string CommandConstructor(string cmd, Output? output = Output.Hidden, string dir = "")
+        {
+            if (!String.IsNullOrEmpty(dir))
+            {
+                dir = $" \"{dir}\"";
+            }
+            if (output == Output.External)
+            {
+                cmd = $"{Directory.GetCurrentDirectory()}/cmd.bat \"{cmd}\"{dir}";
+            }
+            cmd = $"/c \"{cmd}\"";
+
+            return cmd;
+        }
+
+        public static Response Term(string cmd, Output? output = Output.Hidden, string dir = "")
+        {
+            var result = new Response();
+            var stderr = new StringBuilder();
+            var stdout = new StringBuilder();
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = GetFileName();
+                startInfo.Arguments = CommandConstructor(cmd, output, dir);
+                startInfo.RedirectStandardOutput = !(output == Output.External);
+                startInfo.RedirectStandardError = !(output == Output.External);
+                startInfo.UseShellExecute = false;
+                startInfo.CreateNoWindow = !(output == Output.External);
+                startInfo.RedirectStandardInput = true;
+
+                if (!String.IsNullOrEmpty(dir) && output != Output.External)
+                {
+                    startInfo.WorkingDirectory = dir;
+                }
+
+                using (Process process = Process.Start(startInfo))
+                {
+                    switch (output)
+                    {
+                        case Output.Internal:
+                            while (!process.StandardOutput.EndOfStream)
+                            {
+                                try
+                                {
+                                    foreach (ProcessThread thread in process.Threads)
+                                        if (thread.ThreadState == System.Diagnostics.ThreadState.Wait
+                                            && thread.WaitReason == ThreadWaitReason.UserRequest)
+                                        {
+                                            var streamWriter = process.StandardInput;
+                                            streamWriter.WriteLine("I'm supplying input!");
+                                        }
+                                }
+                                catch { }
+                                string line = process.StandardOutput.ReadLine();
+                                stdout.AppendLine(line);
+                                Console.WriteLine(line);
+                            }
+
+                            while (!process.StandardError.EndOfStream)
+                            {
+                                string line = process.StandardError.ReadLine();
+                                stderr.AppendLine(line);
+                                Console.WriteLine(line);
+                            }
+                            break;
+                        case Output.Hidden:
+                            stdout.AppendLine(process.StandardOutput.ReadToEnd());
+                            stderr.AppendLine(process.StandardError.ReadToEnd());
+                            break;
+                    }
+                    process.WaitForExit();
+                    result.stdout = stdout.ToString();
+                    result.stderr = stderr.ToString();
+                    result.code = process.ExitCode;
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+            return result;
+        }
+    }
+}
